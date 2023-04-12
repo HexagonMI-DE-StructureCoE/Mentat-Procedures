9938d6202ba4ef0786c552a9d5c8d4c56c3d061f
FabioScannavino <72655214+fabioHexagon@users.noreply.github.com>
2023-04-12 15:53:06 +0200

    adding

---
+++ CLEAN_OLD_PROC/user1_mm.py
@@ -0,0 +1,8 @@
+from py_mentat import *
+#
+def main():
+    #print("This is a test file to demonstrate the execution of .py files in Mentat.")
+    py_prompt("This is a test file to demonstrate the execution of .py files in Mentat.")
+#
+if __name__ == '__main__' :
+    main()
