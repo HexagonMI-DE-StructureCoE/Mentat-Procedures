8557307d7c255375794d83b04cefbbc0d53a1014
FabioScannavino <72655214+FabioScannavino@users.noreply.github.com>
2023-05-21 21:15:58 +0200

    word
    improving report tools

--- REPORT/report_model.py
+++ REPORT/report_model.py
@@ -3,6 +3,17 @@ from py_mentat import *
 import os
 import sys 
 
+import subprocess
+
+import importlib
+
+def is_package_installed(package_name):
+    try:
+        importlib.import_module(package_name)
+        return True
+    except ImportError:
+        return False
+    
 current_path = os.path.dirname(os.path.abspath(__file__))
 new_path = os.path.join(current_path, "..", "PYTHON_LIB")
 
@@ -12,6 +23,75 @@ if not os.path.exists(new_path):
     print("Please install docx lib") 
 
 sys.path.append(new_path)
+
+
+try:
+    from docx import Document
+    print("ok package is installed.")
+except:
+
+    python_path = sys.executable
+    python_0 = os.path.dirname(python_path)
+    if os.name == "nt":
+        python_home = os.path.join(python_0,"..","..","python","WIN8664")
+    else:
+        # TODO:check
+        python_home = os.path.join(python_0,"..","..","python","lx84")
+
+    if os.name == "nt":
+        fileo = open("installa.bat","w")
+        fileo.write("@echo off\n")
+        fileo.write("\n")
+        fileo.write("set PYTHONPATH=..\PYTHON_LIB\n")
+        fileo.write("set PYTHONHOME=%s\n" % python_home )
+        fileo.write("\n")
+        fileo.write("set PATH=%PYTHONHOME%\PyQt5;%PYTHONHOME%\;%PYTHONHOME%\DLLs;%PYTHONHOME%\Scripts\n")
+        fileo.write("set PATH=%PATH%;c:\Windows;c:\Windows\System32;c:\Windows\Wbem\n")
+        fileo.write("\n")
+        fileo.write("\n")
+        fileo.write("SET SUBDIR=%~dp0\n")
+        fileo.write("cd /D %SUBDIR% \n")
+
+        fileo.write("echo Do you want to execute the next command? (Y/N)\n")
+        fileo.write("choice /c YN\n")
+        fileo.write("\n")
+        fileo.write("if errorlevel 2 (\n")
+        fileo.write("    echo Command execution canceled.\n")
+        fileo.write("exit")
+        fileo.write(") else (\n")
+        fileo.write("    echo Proceeding with the next command.\n")
+
+
+        fileo.write("cd /D %s\n" % new_path)
+        fileo.write("%PYTHONHOME%\\python -m pip install python-docx --target=..\PYTHON_LIB --no-user \n" )
+
+        fileo.write("echo ....\n")
+        fileo.write("echo Exit from Mentat and rerun the tool \n")
+
+
+        fileo.write("pause")
+        fileo.write("exit")
+        fileo.write(")\n")
+
+        fileo.close()
+
+
+
+        cur_file_dir= os.path.dirname(os.path.abspath(__file__))
+        install_location = os.path.join(cur_file_dir,"..","PYTHON_LIB")
+
+        if not os.path.exists(install_location):
+            print("creating folder")
+            os.makedirs(install_location)
+        
+        if os.name == "nt":
+            subprocess.Popen(['start', 'cmd', '/k', 'installa.bat'], shell=True)
+
+    else:
+        print("The python-docx package was not installed.")
+
+
+
 
 from docx import Document
 from docx.shared import Inches
@@ -314,6 +394,13 @@ def main():
     filename = "report_" + str(modelname1) + ".docx"
 
     document.save(filename)
+
+    try:
+        #py_send("*system_command %s" % filename)
+        subprocess.Popen(['start', 'winword', filename], shell=True)
+        pass
+    except:
+        print("no opending file")
 
     py_prompt("Done")
 
